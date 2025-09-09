97ea3569c5af6054c6172950bbe007281d7106df
JOÃO VICTOR SANTOS DE MORAES <25132878@CFP127ALUNO.LOCAL>
2025-09-09 11:26:44 -0300

    projeto paticamente finalizado

---
+++ app.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const input = document.getElementById('textoInput')
+const button = document.getElementById('buscar')
+const lista = document.getElementById('listaDogs')
+
+async function buscarImagens(raca) {
+    let cardsRemovidos = document.querySelectorAll('.containerImg')
+    let i = 0
+
+    cardsRemovidos.forEach(() => {
+        cardsRemovidos[i].parentNode.removeChild(cardsRemovidos[i])
+        i++
+    })
+
+    const url = `https://dog.ceo/api/breed/${raca}/images`
+    const response = await fetch(url)
+    const imagens = await response.json()
+
+    const containerImgs = document.getElementById('containerDog')
+
+    for (let i = 0; i < imagens.message.length; i++) {
+        const containerImg = document.createElement('div')
+        containerImg.classList.add('containerImg')
+        containerImgs.appendChild(containerImg)
+
+        const img = document.createElement('img')
+        img.classList.add('imgDog')
+        img.src = imagens.message[i]
+        containerImg.appendChild(img)
+    }
+
+}
+
+document.addEventListener('click', () => {
+    if (!input.contains(event.target) || lista.contains(event.target)) {
+        lista.style.display = 'none'
+    }
+})
+
+input.addEventListener('click', () => {
+    lista.style.display = 'flex'
+})
+
+button.addEventListener('click', () => {
+    let raca = String(input.value)
+    raca = raca.toLowerCase()
+    buscarImagens(raca)
+})
+
